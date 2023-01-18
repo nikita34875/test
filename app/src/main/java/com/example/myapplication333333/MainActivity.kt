8cfd9ae639d74d187a6306538e50d83150613af0
User <User@zorgespo-13.corp.knrtu.ru>
2023-01-18 18:52:31 +0300

    test1

---
+++ app/src/main/java/com/example/myapplication333333/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication333333
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        setContentView(R.layout.activity_main)
+    }
+}
